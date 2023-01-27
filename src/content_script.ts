c7a0b84319f6f1efa10bc93633be96b127c73a58
Denis Timofijuk <denis.timofijuk@gmail.com>
2023-01-27 14:57:34 +0200

    App is working as intended.

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,5 +1,15 @@
 export {}
 
+const head = document.head || document.getElementsByTagName('head')[0];
+const style = document.createElement('style');
+const css = `
+  .dgtm-blacklist-name { color: #BC2C19 !important; } 
+  .dgtm-blacklist-content { background: #F1A399 !important; }
+  `;
+head.appendChild(style);
+style.appendChild(document.createTextNode(css));
+
+
 const storageList:string[] = [];
 
 chrome.storage.onChanged.addListener(function storageUpdateHandler(changes, areaName){
@@ -9,11 +19,22 @@ chrome.storage.onChanged.addListener(function storageUpdateHandler(changes, area
 
 init();
 
+
+let isScrolling:any;
+window.addEventListener('scroll', function ( event ) {
+	window.clearTimeout( isScrolling );
+	isScrolling = setTimeout(function() {
+		handleList();
+	}, 66);
+}, false);
+
+
 async function init() {
   const list = await loadListFromStorage();
   console.log('DGTM. content loaded.', list);
 
   resetMemoryList(list);
+  handleList();
 }
 
 function resetMemoryList(newList:string[]|undefined) {
@@ -31,7 +52,28 @@ function loadListFromStorage() {
 }
 
 function handleList() {
-  storageList.forEach(item =>{
-    
-  })
+  let counter = 0;
+  collectPageItems().forEach((nodeElement)=>{
+    for(let childNode of nodeElement.childNodes){
+      if(childNode.nodeType == Node.TEXT_NODE){
+        if(childNode.textContent && storageList.indexOf(childNode.textContent) > -1){
+          nodeElement.classList.add('dgtm-blacklist-name');
+          nodeElement.closest('div.x1y1aw1k.xn6708d.xwib8y2.x1ye3gou')?.classList.add('dgtm-blacklist-content');
+          nodeElement.closest('div.x78zum5.xdt5ytf.xz62fqu.x16ldp7u')?.classList.add('dgtm-blacklist-content');
+          break;
+        }
+      }
+    };
+    nodeElement.classList.add('dgtm-checked');
+    counter ++;
+  });
+
+  console.log('Checket Items:', counter);
+}
+
+function collectPageItems() {
+  const collection = document.querySelectorAll('.xzsf02u.x1s688f:not(.dgtm-checked)');
+
+  console.log('Colected Items:', collection.length);
+  return collection
 }
